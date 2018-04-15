017ebcacef23eed02fbf52d3ae3f24f0bfca769b
xophio <latheef.razzaq@gmail.com>
2018-04-15 16:42:07 +0500

    jwt authentication done

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Api\V1\AuthRequest;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use App\User;
+use JWTAuth;
 
 class AuthController extends Controller
 {
@@ -26,8 +27,6 @@ class AuthController extends Controller
 
       $data = json_decode($result->getBody());
 
-      //return $result->getBody();
-
       $userData = [
         'token' => $data->access_token,
         'firstname' => $data->athlete->firstname,
@@ -41,13 +40,42 @@ class AuthController extends Controller
       $user = User::updateOrCreate($userData);
       
       if(!$user) {
-        return response()->json(['code' => 403, 'error' => 'unauthorized', 'message' => '']);
+        return response()->json(['code' => 401, 'error' => 'unauthorized', 'message' => '']);
       }
 
+      $jwt = JWTAuth::fromUser($user, []);
+
+      if($jwt) {
+        return response(['tokent' => $jwt], 200);
+      }
       
+      return response()->json(['code' => 401, 'error' => 'unauthorized', 'message' => '']);
     }
     catch(GuzzleException $e) {
       return response()->json(['code' => $e->getCode(), 'error' => 'unauthorized', 'message' => $e->getMessage()], $e->getCode());
     }
+  }
+
+  public function authenticatedUser(Request $request) {
+    try {
+      if (! $user = JWTAuth::parseToken()->authenticate()) {
+        return response()->json(['user_not_found'], 404);
+      }
+    } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+  
+      return response()->json(['token_expired'], $e->getStatusCode());
+  
+    } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+  
+      return response()->json(['token_invalid'], $e->getStatusCode());
+  
+    } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+  
+      return response()->json(['token_absent'], $e->getStatusCode());
+    }
+
+    unset($user->token);
+    
+    return response()->json(compact('user'));
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::post('auth/authenticate', 'Api\V1\AuthController@authenticate');
+Route::get('auth/me', 'Api\V1\AuthController@authenticatedUser');
