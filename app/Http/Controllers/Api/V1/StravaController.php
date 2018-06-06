32412ab57b48fbcde5c830449469251965a0dd1b
Abdul Latheef <latheef.razzaq@gmail.com>
2018-06-06 08:25:20 +0000

    subscription and view subscription done

--- app/Http/Controllers/Api/V1/StravaController.php
+++ app/Http/Controllers/Api/V1/StravaController.php
@@ -10,7 +10,7 @@ class StravaController extends BaseController
 {
   public function subscribe() {
     $client = new Client();
-    $result = $client->get(env('STRAVA_WEBHOOK_URL'),
+    $result = $client->post(env('STRAVA_WEBHOOK_URL'),
       ['form_params' =>
         [
           'client_id' => env('STRAVA_CLIENT_ID'),
@@ -28,14 +28,11 @@ class StravaController extends BaseController
 
   public function view() {
     $client = new Client();
-    $result = $client->get(env('STRAVA_WEBHOOK_URL'),
-      ['form_params' =>
-        [
-          'client_id' => env('STRAVA_CLIENT_ID'),
-          'client_secret' => env('STRAVA_CLIENT_SECRET')
-        ]
-      ]
-    );
+
+    $params['client_id'] = env('STRAVA_CLIENT_ID');
+    $params['client_secret'] = env('STRAVA_CLIENT_SECRET');
+    $url = env('STRAVA_WEBHOOK_URL') . '?' .http_build_query($params);
+    $result = $client->get($url, []); 
 
     $data = json_decode($result->getBody());
 
@@ -43,14 +40,14 @@ class StravaController extends BaseController
   }
 
   public function getHook(Request $request) {
-    if(!$request->get('hub.verify_token')) {
+    if(!$request->get('hub_verify_token')) {
       return $this->respondWithError(null, 401, 'unauthorized');
     }
-    if($request->get('hub.verify_token') !== env('STRAVA_WEBHOOK_VERIFY_TOKEN')) {
+    if($request->get('hub_verify_token') !== env('STRAVA_WEBHOOK_VERIFY_TOKEN')) {
       return $this->respondWithError(null, 401, 'unauthorized');
     }
-    if($request->get('hub.verify_token')) {
-      return response()->json(['hub.challenge' => $request->get('hub.verify_token')], 200);
+    if($request->get('hub_verify_token')) {
+      return response()->json(['hub.challenge' => $request->get('hub_challenge')], 200);
     }
 
     return $this->respondWithError(null, 401, 'unauthorized');
