ff3f5a75f3e07c7d364bc52a9424d77b02dda706
xophio <latheef.razzaq@gmail.com>
2018-06-06 12:46:17 +0500

    readying for subscription

--- app/Http/Controllers/Api/V1/StravaController.php
+++ app/Http/Controllers/Api/V1/StravaController.php
@@ -9,7 +9,17 @@ use GuzzleHttp\Client;
 class StravaController extends BaseController
 {
   public function subscribe() {
-
+    $client = new Client();
+    $result = $client->get(env('STRAVA_WEBHOOK_URL'),
+      ['form_params' =>
+        [
+          'client_id' => env('STRAVA_CLIENT_ID'),
+          'client_secret' => env('STRAVA_CLIENT_SECRET'),
+          'callback_url' => env('STRAVA_WEBHOOK_CALLBACK_URL'),
+          'verify_token' => env('STRAVA_WEBHOOK_VERIFY_TOKEN')
+        ]
+      ]
+    );
   }
 
   public function view() {
@@ -18,7 +28,7 @@ class StravaController extends BaseController
       ['form_params' =>
         [
           'client_id' => env('STRAVA_CLIENT_ID'),
-          'client_secret' => env('STRAVA_CLIENT_SECRET')       
+          'client_secret' => env('STRAVA_CLIENT_SECRET')
         ]
       ]
     );
@@ -26,5 +36,23 @@ class StravaController extends BaseController
     $data = json_decode($result->getBody());
 
     return $this->respond($data);
+  }
+
+  public function getHook(Request $request) {
+    if(!$request->get('hub.verify_token')) {
+      return $this->respondWithError(null, 401, 'unauthorized');
+    }
+    if($request->get('hub.verify_token') !== env('STRAVA_WEBHOOK_VERIFY_TOKEN')) {
+      return $this->respondWithError(null, 401, 'unauthorized');
+    }
+    if($request->get('hub.verify_token')) {
+      return response()->json(['hub.challenge' => $request->get('hub.verify_token')], 200);
+    }
+
+    return $this->respondWithError(null, 401, 'unauthorized');
+  }
+
+  public function postHook(Request $request) {
+    
   }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,6 @@ class Kernel extends HttpKernel
 
         'strava' => [
           'throttle:300,1',
-          'stv',
         ],
     ];
 
