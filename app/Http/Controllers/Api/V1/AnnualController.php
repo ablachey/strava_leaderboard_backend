1cdebe84b09116b947f2d61928914ee3d9c36e17
xophio <latheef.razzaq@gmail.com>
2018-06-29 19:16:07 +0500

    annual stats added

--- app/Http/Controllers/Api/V1/AnnualController.php
+++ app/Http/Controllers/Api/V1/AnnualController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Api\BaseController;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Auth;
+use \Carbon\Carbon;
+use DB;
 
-class AnnualController extends Controller
+class AnnualController extends BaseController
 {
-    //
+  public function getStats() {
+    $user = Auth::user();
+    $firstDay = new Carbon('first day of january');
+    $lastDay = new Carbon('last day of december');
+    
+    $activities = $user->activities()
+                    ->where('type', 'run')
+                    ->where('start_date_local', '>=', $firstDay)
+                    ->where('start_date_local', '<=', $lastDay);
+    
+    $runs = $activities->count();
+    $distTime = $activities->select(DB::raw('SUM(distance) as total_distance, SUM(elapsed_time) as total_time'))->first();
+
+    $res['total_runs'] = $runs;
+    $res['total_distance'] = $distTime['total_distance'];
+    $res['total_time'] = $distTime['total_time'];
+
+    return $this->respond($res);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,5 @@ Route::group(['middleware' => 'jwt.auth'], function() {
 
   Route::get('prs/{id}', 'Api\V1\PRController@show');
   Route::get('activities/recent', 'Api\V1\ActivityController@index');
-  
+  Route::get('annual/stats', 'Api\V1\AnnualController@getStats');
 });
